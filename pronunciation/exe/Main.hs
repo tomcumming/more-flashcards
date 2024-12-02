864a5901ea94d37b553165e9aeba0ff99b00c2f3
Tom Cumming <mail@tomcumming.co.uk>
2024-12-02 22:55:50 +0000

    Pronunciation deck

---
+++ pronunciation/exe/Main.hs
@@ -0,0 +1,64 @@
+module Main (main) where
+
+import Control.Category ((>>>))
+import Control.Monad (forM_)
+import Data.Bool (bool)
+import Data.Char (isSpace)
+import Data.Foldable (fold, toList)
+import Data.Function ((&))
+import Data.List qualified as List
+import Data.List.NonEmpty qualified as NE
+import Data.Map qualified as M
+import Data.Ord (comparing)
+import Data.Sequence qualified as Sq
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+import DataLoading (WordDef (..), interleave, loadHskWords, loadTocflWords, shuffleGroups, uniqueBy, wdTxt)
+
+putRowLn :: [T.Text] -> IO ()
+putRowLn = List.intersperse "\t" >>> fold >>> T.putStrLn
+
+boolean :: Bool -> T.Text
+boolean = bool "0" "1"
+
+main :: IO ()
+main = do
+  hskWords <- loadHskWords & fmap shuffleGroups
+  tocflWords <- loadTocflWords & fmap shuffleGroups
+
+  let allWords =
+        interleave (toList hskWords) (toList tocflWords)
+          & zip [0 :: Int ..]
+          & fmap (\(n, wd) -> (wdTxt wd, (n, NE.singleton wd)))
+          & M.fromListWith
+            (\(n, d) (m, e) -> (min n m, d <> e))
+          & toList
+          & List.sortOn fst
+          & fmap snd
+          & Sq.fromList
+
+  ["Word", "Simp", "Trad", "Pinyin", "English"] & putRowLn
+
+  forM_ allWords $ \wds -> do
+    let word = NE.head wds & wdTxt
+    let isSimp = any (wdTrad >>> not) wds & boolean
+    let isTrad = any wdTrad wds & boolean
+    let pinyin =
+          wds
+            & fmap (wdPinyin >>> normalisePinyin)
+            & toList
+            & uniqueBy id
+            & List.intersperse ", "
+            & fold
+    let english =
+          wds
+            & fmap wdEng
+            & NE.sortBy (comparing T.length)
+            & NE.intersperse "; "
+            & fold
+
+    [word, isSimp, isTrad, pinyin, english]
+      & putRowLn
+
+normalisePinyin :: T.Text -> T.Text
+normalisePinyin = T.filter (isSpace >>> not)
--- stroke-order/exe/Main.hs
+++ stroke-order/exe/Main.hs
@@ -1,72 +1,19 @@
 module Main (main) where
 
 import Control.Category ((>>>))
-import Control.Monad (when, (>=>))
-import Data.Bifunctor (second)
+import Control.Monad (when)
 import Data.Foldable (fold, forM_, toList)
 import Data.Function ((&))
 import Data.List qualified as L
 import Data.List.NonEmpty qualified as NE
 import Data.Map.Strict qualified as M
-import Data.Maybe (catMaybes, fromMaybe)
-import Data.Sequence qualified as Seq
-import Data.Set qualified as S
+import Data.Maybe (catMaybes)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
+import DataLoading (WordDef (..), interleave, loadHskWords, loadTocflWords, shuffleGroups, uniqueBy)
 import System.Directory (doesFileExist)
 import System.IO (stderr)
-import System.Random (StdGen, mkStdGen, uniformR)
-import Text.Read qualified as T
 import Unicode.Char.General.Scripts qualified as UC
-
-data WordDef = WordDef
-  { wdTxt :: !T.Text,
-    wdPinyin :: !T.Text,
-    wdEng :: !T.Text,
-    wdTrad :: !Bool
-  }
-  deriving (Show)
-
-parseHskRow :: T.Text -> IO (Int, WordDef)
-parseHskRow =
-  T.splitOn "\t" >>> \case
-    [lvlTxt, wdTxt, wdPinyin, _, wdEng]
-      | Just lvl <- T.readMaybe (T.unpack lvlTxt) -> (lvl, WordDef {wdTrad = False, ..}) & pure
-    cells -> "Could not parse" <> show cells & fail
-
-stripQuotes :: T.Text -> IO T.Text
-stripQuotes =
-  (T.stripSuffix "\"" >=> T.stripPrefix "\"")
-    >>> maybe (fail "Could not strip quotes") pure
-
-parseTocflRow :: T.Text -> IO (Int, WordDef)
-parseTocflRow =
-  T.splitOn "\t"
-    >>> traverse stripQuotes
-    >=> \case
-      [wdTxt, wdPinyin, _, lvlTxt, wdEng, _]
-        | Just lvl <- T.readMaybe (T.unpack lvlTxt) -> (lvl, WordDef {wdTrad = True, ..}) & pure
-      cells -> "Could not parse" <> show cells & fail
-
-loadHskWords :: IO (Seq.Seq (Seq.Seq WordDef))
-loadHskWords = do
-  ls <-
-    T.readFile "stroke-order/data/hsk-word-list.tsv"
-      & fmap (T.lines >>> drop 1) -- skip headings
-  lvls <-
-    traverse parseHskRow ls
-      & fmap (fmap (second Seq.singleton) >>> M.fromListWith (<>))
-  M.elems lvls & Seq.fromList & pure
-
-loadTocflWords :: IO (Seq.Seq (Seq.Seq WordDef))
-loadTocflWords = do
-  ls <-
-    T.readFile "stroke-order/data/tocfl.tsv"
-      & fmap (T.lines >>> drop 1) -- skip headings
-  lvls <-
-    traverse parseTocflRow ls
-      & fmap (fmap (second Seq.singleton) >>> M.fromListWith (<>))
-  M.elems lvls & Seq.fromList & pure
 
 maskHints :: WordDef -> WordDef
 maskHints wd = wd {wdEng = T.map go (wdEng wd)}
@@ -74,34 +21,6 @@ maskHints wd = wd {wdEng = T.map go (wdEng wd)}
     go c = case UC.script c of
       UC.Han -> '？'
       _ -> c
-
-shuffleGroups :: forall a. Seq.Seq (Seq.Seq a) -> Seq.Seq a
-shuffleGroups = fmap (shuffleGroup stdGen) >>> fold
-  where
-    stdGen = mkStdGen 0
-
-    shuffleGroup :: StdGen -> Seq.Seq a -> Seq.Seq a
-    shuffleGroup g' = \case
-      Seq.Empty -> mempty
-      s ->
-        let (i, g'') = uniformR (0, Seq.length s & pred) g'
-            x = s Seq.!? i & fromMaybe (error "OOB")
-            s' = Seq.deleteAt i s
-         in x Seq.<| shuffleGroup g'' s'
-
-uniqueBy :: (Ord b) => (a -> b) -> [a] -> [a]
-uniqueBy f = go mempty
-  where
-    go seen = \case
-      x : xs | f x `S.notMember` seen -> x : go (S.insert (f x) seen) xs
-      _ : xs -> go seen xs
-      [] -> []
-
-interleave :: [a] -> [a] -> [a]
-interleave xs ys = case (xs, ys) of
-  (x : xs', y : ys') -> x : y : interleave xs' ys'
-  ([], _) -> ys
-  (_, []) -> xs
 
 findStrokeData :: T.Text -> IO (Maybe (T.Text, T.Text))
 findStrokeData c = do
